c02747cf5ac07994e7f62f36fde73e94adce5362
Alexander Doroshenko <ado@sitecore.net>
2014-09-11 16:39:44 +0300

    Fixed build script.

--- buildscript/run.ps1
+++ buildscript/run.ps1
@@ -1,8 +1,7 @@
 Clear-Host
 
 remove-module [p]sake
-$ScriptDir = Split-Path -parent $MyInvocation.MyCommand.Path
-import-module $ScriptDir\Tools\Psake\psake.psm1
+import-module .\Tools\psake\psake.psm1
 
 $psake.use_exit_on_error = $true 
-Invoke-psake $ScriptDir\build.ps1 Package -properties @{ buildNumber = '123'; }
+Invoke-psake .\build.ps1 Package -properties @{ buildNumber = '123'; }
